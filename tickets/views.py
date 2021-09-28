e7bf57a215c5315df4f0185b041f08060018146e
Alexei Glon <alexglon@outlook.com>
2021-09-28 19:12:58 +0300

    Tickets responded to by a support member view implemented, Ticket views URLs fixed

--- tickets/views.py
+++ tickets/views.py
@@ -19,7 +19,7 @@ class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
-    @action(methods=['GET'], detail=False, url_path='tickets_by_user/(?P<user>[^/.]+)')
+    @action(methods=['GET'], detail=False, url_path='by_user/(?P<user>[^/.]+)')
     def tickets_by_user(self, request, user):
         """Returns all tickets created by a user with specified ID."""
         # TODO: rewrite this using filter (that'd be defined after serializer_class)?
@@ -30,10 +30,20 @@ class TicketViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(tickets, many=True)
         return Response(serializer.data)
 
-    @action(methods=['GET'], detail=False, url_path='tickets_by_status/(?P<ticket_status>[^/.]+)')
+    @action(methods=['GET'], detail=False, url_path='by_status/(?P<ticket_status>[^/.]+)')
     def tickets_by_status(self, request, ticket_status):
         """Returns all tickets that have the specified status."""
         tickets = Ticket.objects.filter(status=ticket_status)
+        if not tickets:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        serializer = self.get_serializer(tickets, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['GET'], detail=False, url_path='by_support_member/(?P<support>[^/.]+)')
+    def tickets_by_support_member(self, request, support):
+        """Returns all tickets that have been responded to by the specified support member."""
+        tickets = Ticket.objects.filter(response__support_member=support)
         if not tickets:
             return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -47,7 +57,7 @@ class TicketViewSet(viewsets.ModelViewSet):
 
         # `partial=True` allows custom {"status": "foobar"} JSONs to be used
         # `context={'request': request}` is required by `HyperlinkedIdentityField`
-        serializer = TicketSerializer(ticket, context={'request': request},
+        serializer = self.get_serializer(ticket, context={'request': request},
                                       data=request.data, partial=True)
         if serializer.is_valid():
             serializer.validated_data['status'] = request.data['status']
@@ -57,7 +67,6 @@ class TicketViewSet(viewsets.ModelViewSet):
 
 
 # TODO: implement status check logic (e.g. user can't comment on a closed ticket)
-# TODO: view that shows all tickets answered by a specified support member
 # TODO: permissions on every ticket
 # TODO:
 # TODO:
